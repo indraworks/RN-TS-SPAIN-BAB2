e65264bd8699500f321f24314a87bbba74d0d703
indraworks <indra.works2020@gmail.com>
2023-05-01 15:21:23 +0700

    bab2- making HelloWorld Screen

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HelloWorldScreen } from './src/screens/HelloWorldScreen'
+
+
+export const App = () => {
+  return (
+    <HelloWorldScreen/>
+  )
+    
+}
+
+/*
+View mirip sama dgn div tanpa ada pengaturan dia ngatur mulai 0,0 kiri atas (top)
+stiap tulisan harus di taruh diatas Text ,nah utk RN slalu atur pakai flexBox! 
+
+
+
+*/
